09b663341a3f7552ee26c111a3bd3ea2266733f6
NilsonLZN <nilson_lzn@hotmail.com>
2024-11-20 03:00:58 -0300

    Desafio concluídogit add Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "99970-7070", modelo: "Nokia N20", imei: "352512345600001", memoria: 128);
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Facebook");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "99960-6060", modelo: "Iphone 15", imei: "352512345600002", memoria: 256);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
